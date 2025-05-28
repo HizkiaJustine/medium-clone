759efae437a09eeedf443c8e5d910b83bab0dcf0
HizkiaJustine <zeusxdhd@gmail.com>
2025-05-28 10:57:15 +0700

    fix post image

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -19,7 +19,7 @@
         </a>
     </div>
     <a href="#">
-        <img class="rounded-r-lg w-48 h-full object-cover" src="https://flowbite.com/docs/images/blog/image-1.jpg"
+        <img class="rounded-r-lg w-48 h-full object-cover" src="{{ Storage::url($post->image) }}"
             alt="" />
     </a>
 </div>
